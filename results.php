8616c9a35db91e329c31ff6d728ac77492a34d8a
Alex Yelienok <aelenok@gmail.com>
2024-10-25 23:29:51 +0200

    дрібні зміни стилів

--- results.php
+++ results.php
@@ -6,26 +6,25 @@ if (file_exists($filename)) {
     $totalVotes = array_sum($data['votes']);
 
     echo "<table style='border-collapse: collapse; width: 50%; margin: 0 auto;'>";
-    echo "<tr><th style='border: 1px solid black; padding: 8px;'>Мова</th><th style='border: 1px solid black; padding: 8px;'>Голосів (%)</th><th style='border: 1px solid black; padding: 8px;'>Відсоток</th></tr>";
-    foreach ($data['votes'] as $language => $votes)
-    {
+    echo "<tr><th style='border: 1px solid black; padding: 8px;'>Мова</th><th style='border: 1px solid black; padding: 8px;'>Голосів (%)</th></tr>";
+    foreach ($data['votes'] as $language => $votes) {
         $percentage = round(($votes / $totalVotes) * 100, 2);
         echo "<tr>
                 <td style='border: 1px solid black; padding: 8px;'>$language</td>
                 <td style='border: 1px solid black; padding: 8px;'>
-                    <div style='width: {$percentage}%; background-color: #4CAF50; height: 15px;'>&nbsp;</div>
+                    <div style='width: 100%; height: 40px; position: relative;'>
+                        <div style='width: {$percentage}%; background-color: forestgreen; height: 100%; position: absolute;'>
+                            <span style='position: absolute; left: 65%; top: 50%; transform: translate(-50%, -50%); color: black;'>$percentage%</span>
+                        </div>
+                    </div>
                 </td>
-                <td style='border: 1px solid black; padding: 8px;'>$percentage%</td>
               </tr>";
     }
 
     echo "</table>";
 
     echo "<br>" . "Загальна кількість проголосувавших: {$totalVotes}";
-}
-else
-{
+} else {
     echo "Голоси відсутні.";
 }
 ?>
-
